45032358f03d620f64fb4a050427bd4ee2d52d16
Saurabh Dabas <developerdabas@gmail.com>
2022-03-23 05:04:26 +0530

    Minor Fix

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -7,7 +7,7 @@ export default function DayListItem(props) {
     'day-list__item--full':props.spots === 0
     });
   return (
-    <li className={dayClass} onClick={() => props.setDay(props.id)}>
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2> 
       <h3 className="text--light">{props.spots} spots remaining</h3>
     </li>
