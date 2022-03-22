32d6c0132c3a019da71a82ccbf19b7d0360f91b9
Saurabh Dabas <developerdabas@gmail.com>
2022-03-22 22:52:47 +0530

    Add a DayList component

---
+++ src/components/DayList.js
@@ -0,0 +1,50 @@
+import React from "react";
+import "components/DayListItem.scss";
+import DayListItem from "components/DayListItem";
+
+// export default function DayList(props){
+
+//   return(
+//     <ul>
+//       <DayListItem 
+//         key={props.days[0].id}
+//         name={props.days[0].name} 
+//         spots={props.days[0].spots} 
+//         selected={props.days[0].name === props.day}
+//         setDay={props.setDay}  
+//       />
+//       <DayListItem
+//         key={props.days[1].id} 
+//         name={props.days[1].name} 
+//         spots={props.days[1].spots} 
+//         selected={props.days[1].name === props.day}
+//         setDay={props.setDay}  
+//       />
+//       <DayListItem 
+//         key={props.days[2].id}
+//         name={props.days[2].name}
+//         spots={props.days[2].spots} 
+//         selected={props.days[2].name === props.day}
+//         setDay={props.setDay}  
+//       />      
+//     </ul>
+//   )
+// }
+export default function DayList(props){
+
+  const DayListItems = props.days.map((day)=>{
+    return (
+      <DayListItem 
+       key={day.id}
+        name={day.name} 
+        spots={day.spots} 
+        selected={day.name === props.day}
+        setDay={props.setDay}  
+      />
+    );
+  })
+  return(
+    <ul>{DayListItems}</ul>
+  )
+
+}
