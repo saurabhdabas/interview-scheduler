4a542a02811b83c2d93b521ceca1b7c14715bdf3
Saurabh Dabas <developerdabas@gmail.com>
2022-03-24 00:47:45 +0530

    Destructured props

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -3,24 +3,24 @@ import InterviewerListItem from "components/InterviewerListItem";
 import "components/InterviewList.scss";
 
 
-export default function InterviewerList(props) {
-  console.log("propsInterviewerList:",props);
-  const interviewers = props.interviewers.map((interviewer)=>{
+export default function InterviewerList({interviewers,selectedInterviewer,setInterviewer}) {
+  // We have destructured the prop directly here, to avoid using props in every single line.
+  const interviewersList = interviewers.map((interviewer)=>{
     // console.log(interviewer);
     return (
     <InterviewerListItem
     key={interviewer.id} // Important to note: We need this to avoid a key prop error for child.
     name={interviewer.name} 
     avatar={interviewer.avatar} 
-    selected={interviewer.id === props.interviewer}//selected={interviewer.id === props.interviewer}
-    setInterviewer={()=> props.setInterviewer(interviewer.id)}//setInterviewer={()=> props.setInterviewer(interviewer.id)}
+    selected={interviewer.id === selectedInterviewer}//selected={interviewer.id === interviewer}
+    setInterviewer={()=> setInterviewer(interviewer.id)}
     />
     );
   });
   return (
     <section className="interviewers">
     <h4 className="interviewers__header text--light">Interviewer</h4>
-    <ul className="interviewers__list">{interviewers}</ul>
+    <ul className="interviewers__list">{interviewersList}</ul>
   </section>
   ); 
 }
