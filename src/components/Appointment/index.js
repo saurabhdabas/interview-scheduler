c12e5c584089cc1912302287c925718ac2628b13
Saurabh Dabas <developerdabas@gmail.com>
2022-03-24 03:12:06 +0530

    Add Header

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { Fragment }  from "react";
+
 import "components/Appointment/styles.scss";
-export default function Appointment(props) {
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
+export default function Appointment({id,time,interview}) {
+  console.log("propsFromAppointment:",id,time,interview)
   return (
-    <article className="appointment">{props.time ? `Appointment at ${props.time}` : "No Appointments"}</article>
+  <article className="appointment">
+    <Header time={time}/>
+    {interview ? <Show student={interview.student} interviewer={interview.interviewer.name}/> : <Empty/> }
+  </article>
+
   );
 }
+
+{/* <article className="appointment">{props.time ? `Appointment at ${props.time}` : "No Appointments"}</article> */}
