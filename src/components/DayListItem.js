49f94c72b138c22a352503bc7d155a5e1c6e41dc
Saurabh Dabas <developerdabas@gmail.com>
2022-03-24 00:49:15 +0530

    Minor fix

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -2,14 +2,15 @@ import React from "react";
 import classNames from "classnames";
 import "components/DayListItem.scss";
 export default function DayListItem(props) {
+  console.log(props)
   let dayClass = classNames('day-list__item',{
     'day-list__item--selected':props.selected,
     'day-list__item--full':props.spots === 0
     });
   return (
-    <li className={dayClass} onClick={props.setDay} selected={props.selected}>
-      <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+    <li className={dayClass} onClick={()=>props.setDay(props.name)} selected={props.selected}>
+    <h2 className="text--regular">{props.name}</h2> 
+    <h3 className="text--light">{props.spots} spots remaining</h3>
     </li>
   );
 }
