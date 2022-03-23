564e5f3d674dc14c8558df77700026b6f9cfed74
Saurabh Dabas <developerdabas@gmail.com>
2022-03-24 03:08:57 +0530

    Iterate over appointments Array and mounted Appointment component

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import DayList from "components/DayList";
+import Appointment from "components/Appointment";
 import "components/Application.scss";
 
 const days = [
@@ -20,8 +21,57 @@ const days = [
   },
 ];
 
-export default function Application(props) {
+const appointments = {
+  "1": {
+    id: 1,
+    time: "12pm",
+  },
+  "2": {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer:{
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  "3": {
+    id: 3,
+    time: "2pm",
+  },
+  "4": {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer:{
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  "5": {
+    id: 5,
+    time: "4pm",
+  }
+}
+
+export default function Application() {
   const [day,setDay] = useState("Monday");
+  const appointmentList = Object.values(appointments).map((appointment)=>{
+    return (
+      <Appointment
+      key={appointment.id}
+      id={appointment.id}
+      time={appointment.time}
+      interview={appointment.interview}
+      />
+    );
+  });
   return (
     <main className="layout">
       <section className="sidebar">
@@ -35,7 +85,8 @@ export default function Application(props) {
       </nav>
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        <ul>{appointmentList}</ul>
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
