38cb9001411ea59db997a5cb915d7e9daff68801
Saurabh Dabas <developerdabas@gmail.com>
2022-03-30 02:00:37 +0530

    Add a validation fucntion

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -6,6 +6,20 @@ export default function Form(props) {
   
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState('');
+
+	const validate = () => {
+		if (student === '') {
+			setError('Student name cannot be blank');
+			return;
+		}
+		if (interviewer === null) {
+			setError('You must choose an interviewer');
+			return;
+		}
+		setError('');
+		props.onSave(student, interviewer);
+	};
   const reset = () => {
     setStudent("");
     setInterviewer(null);
@@ -24,15 +38,18 @@ export default function Form(props) {
             type="text"
             placeholder="Enter Student Name"
             value={student}
-            onChange={(event)=>setStudent(event.target.value)}
+            onChange={(event)=>
+              setStudent(event.target.value)}
+            data-testid="student-name-input"
           />
+          <section className="appointment__validation">{error}</section>
         </form>
       <InterviewerList interviewers={props.interviewers} selectedInterviewer={interviewer} setInterviewer={setInterviewer}/>
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={cancel}>Cancel</Button>
-          <Button confirm onClick={()=>props.onSave(student,interviewer)}>Save</Button>
+          <Button confirm onClick={validate}>Save</Button>
         </section>
       </section>
     </main>
